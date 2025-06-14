30a73b305fe50c775f9713d9d323a59a74888e4c
Asus <mehhussumon47@gmail.com>
2025-06-14 10:21:49 +0600

    Added a file

---
+++ 30 Days of Code/Day_1.py
@@ -0,0 +1,18 @@
+i = 4
+d = 4.0
+s = 'HackerRank'
+# Declare second integer, double, and String variable.
+num1 = int(input())
+num2 = float(input())
+str1 = input()
+# Read and save an integer, double, and String to your variables.
+num1 = num1 + i
+num2 = num2 + d
+# Print the sum of both integer variables on a new line.
+print(num1)
+# Print the sum of the double variables on a new line.
+print(num2)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(f"{s} {str1}")
+ 
