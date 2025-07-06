05b64a1742ba7425122a5453caca04691cb35db5
Asus <mehhussumon47@gmail.com>
2025-07-06 22:38:01 +0600

    Updated a file in 06/07/2025

--- 10 Days of Statistics/Day_0(1).py
+++ 10 Days of Statistics/Day_0(1).py
@@ -1,25 +1,23 @@
-import statistics
-
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-n = int(input())
-number = input().split()
-arr =[]
-for i in range(n):
-    arr.append(int(number[i]))
-
-mean = statistics.mean(arr)
-median = statistics.median(arr)
 
 from collections import Counter
 
-freq = Counter(arr)
-freq_values = list(freq.values())
+# Enter your code here. Read input from STDIN. Print output to STDOUT\
+n = int(input())
+arr = list(map(int, input().split()))
 
-if all(val == freq_values[0] for val in freq_values):
-    mode = min(arr)
+mean = sum(arr) / n
+
+arr.sort()
+if n % 2 == 0:
+    median = (arr[n//2 - 1] + arr[n//2]) / 2
 else:
-    mode = statistics.mode(arr)
+    median = arr[n//2]
 
-print(mean)
-print(median)
+freq = Counter(arr)
+max_freq = max(freq.values())
+modes = [k for k, v in freq.items() if v == max_freq]
+mode = min(modes)
+
+print(f"{mean:.1f}")
+print(f"{median:.1f}")
 print(mode)
