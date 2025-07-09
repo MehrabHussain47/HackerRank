13ae939edf9090492c8e4513a1a83f52a50d558c
Asus <mehhussumon47@gmail.com>
2025-07-10 02:48:57 +0600

    Modified a file in 10/07/2025

--- 10 Days of Statistics/Day_1(2).py
+++ 10 Days of Statistics/Day_1(2).py
@@ -16,6 +16,33 @@ import sys
 
 def interQuartile(values, freqs):
     # Print your answer to 1 decimal place within this function
+    S = []
+    for i in range(len(values)):
+        S.extend([values[i]] * freqs[i])
+    
+    S.sort()
+
+    def median(arr):
+        n = len(arr)
+        mid = n // 2
+        if n % 2 == 0:
+            return (arr[mid - 1] + arr[mid]) / 2
+        else:
+            return arr[mid]
+
+    n = len(S)
+    if n % 2 == 0:
+        lower_half = S[:n // 2]
+        upper_half = S[n // 2:]
+    else:
+        lower_half = S[:n // 2]
+        upper_half = S[n // 2 + 1:]
+
+    Q1 = median(lower_half)
+    Q3 = median(upper_half)
+
+    IQR = round(Q3 - Q1, 1)
+    print(IQR)
 
 if __name__ == '__main__':
     n = int(input().strip())
